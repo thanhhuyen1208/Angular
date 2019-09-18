1fdb23c9daa487f0d6624d302298841141966c6e
thanhhuyen1208 <lunachris1208@gmail.com>
2019-09-18 18:01:43 +0700

    fix path in server.js, dis/index => dist/bookstore/index

--- server.js
+++ server.js
@@ -5,12 +5,12 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dis'));
+app.use(express.static(__dirname + '/dist/bookstore'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/dis/index.html'));
+res.sendFile(path.join(__dirname+'/dist/bookstore/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 4200);
+app.listen(process.env.PORT || 3000);
